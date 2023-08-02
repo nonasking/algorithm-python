db1d56e94fb28e2afde4cfaa3079504539d86e72
nonasking <nonasking@gmail.com>
2023-08-02 15:58:20 +0900

    add basic formula of sort

---
+++ sort/sort.py
@@ -0,0 +1,9 @@
+a = [4, 3, 1, 2, 5]
+
+# 정렬된 새 배열 생성
+sorted_a = a.sorted() # 1 2 3 4 5
+reverse_sorted_a = a.sorted(reverse=True) # 5 4 3 2 1
+
+# 원본 배열 정렬(변경)
+a.sorted() # 1 2 3 4 5
+a.sorted(reverse=True) # 5 4 3 2 1
