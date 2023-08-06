3eac2ae9d36a254a3b5c1f7cc1f6da406be57a58
nonasking <nonasking@gmail.com>
2023-08-06 15:21:51 +0900

    add explanation

--- sort/sort.py
+++ sort/sort.py
@@ -1,3 +1,4 @@
+# 기본 정렬 메서드
 a = [4, 3, 1, 2, 5]
 
 # 정렬된 새 배열 생성
@@ -7,3 +8,8 @@ reverse_sorted_a = a.sorted(reverse=True) # 5 4 3 2 1
 # 원본 배열 정렬(변경)
 a.sorted() # 1 2 3 4 5
 a.sorted(reverse=True) # 5 4 3 2 1
+
+# swap
+array = ['a', 'b']
+array[0], array[1] = array[1], array[0]
+print(array) # ['b', 'a']
