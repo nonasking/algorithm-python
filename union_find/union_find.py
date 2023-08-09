2752243e0f4e392421d16fcdacb62bd6e1bab5f8
nonasking <nonasking@gmail.com>
2023-08-09 16:22:32 +0900

    add union_find

---
+++ union_find/union_find.py
@@ -0,0 +1,41 @@
+# 특정 원소가 속한 집합을 찾는 함수
+def find_parent(parent, x):
+	# 부모 노드가 아니라면, 부모 노드를 찾을 때까지 재귀 호출
+    if parent[x] != x:
+    	parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합 합치기
+def union_parent(parent, a, b):
+	a = find_parent(parent, a)
+	b = find_parent(parent, b)
+    if a < b:
+    	parent[b] = a
+    else:
+    	parent[a] = b
+
+# 노드의 개수, 간선의 개수 입력받기
+v, e = map(int, input().split())
+# 부모 테이블 초기화
+parent = [0] * (v+1) 
+
+# 부모 테이블상에서 부모를 자기 자신으로 초기화
+for i in range(1, v+1):
+	parent[i] = i
+
+# 각 원소에 대해 union 함수 실행
+for i in range(e):
+	a, b = map(int,input().split())
+    union_parent(parent, a, b)
+
+# 각 원소가 속한 집합 출력
+print('각 원소가 속한 집합은: ', end='')
+for i in range(1, v+1):
+	print(find_parent(parent,i), end=' ')
+
+print()
+
+# 부모 테이블 내용 출력
+print('부모 테이블은: ', end='')
+for i in range(1, v+1):
+	print(parent[i], end=' ')
