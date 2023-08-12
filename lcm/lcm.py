31c4819c14e26b4b01e0a714a684197a21818691
nonasking <nonasking@gmail.com>
2023-08-12 10:51:13 +0900

    add solution

---
+++ lcm/lcm.py
@@ -0,0 +1,8 @@
+# a와 b의 최소공배수
+def solution(a, b):
+    max_num = a if a > b else b
+    for i in range(max_num, a * b + 1):
+        if i % a == 0 and i % b == 0:
+            answer = i
+            break
+    return print(answer)
