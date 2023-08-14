87687c5f8910885d060c452f4859a629f4877c17
nonasking <nonasking@gmail.com>
2023-08-14 16:17:30 +0900

    add pre-order

--- tree/tree.py
+++ tree/tree.py
@@ -4,3 +4,11 @@ class Node:
         self.data = data
         self.left_node = left_node
         self.right_node = right_node
+
+# 전위 순회(pre-order traverse)
+def pre_order(node):
+    print(node.data, end=' ')
+    if node.left_node !== None:
+        pre_order(tree[node.left_node])
+    if node.right_node !== None:
+        pre_order(tree[node.right_node])
