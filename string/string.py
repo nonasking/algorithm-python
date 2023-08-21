20df60a11a787728e13a61d260fad20e29e43fb8
nonasking <nonasking@gmail.com>
2023-08-21 17:15:09 +0900

    add string

---
+++ string/string.py
@@ -0,0 +1,12 @@
+# 문자열의 모든 알파벳을 대문자로 변환
+word = 'abc'
+print(word.upper()) # ABC
+
+# 문자열의 첫 글자를 대문자로 변환
+word = 'abc'
+print(word.capitalize()) # Abc
+
+# 문자열의 모든 알파벳을 소문자로 변환
+word = 'ABC'
+print(word.lower()) # abc
+
