ddb44bcf3107a80d42891192b19472ccb92fd7fc
nonasking <nonasking@gmail.com>
2023-08-22 15:56:56 +0900

    add modules

--- calculation/calculation.py
+++ calculation/calculation.py
@@ -10,3 +10,18 @@ print(round(0.6)) # 1
 # 원래 수와 올림/버림했을 때의 차이가 같은 경우 짝수 값 반환
 print(round(0.5)) # 0
 print(round(1.5)) # 2
+
+# 소수점 올림
+# 해당 값에서 + 방향으로 가장 가까운 정수 반환
+import math
+print(math.ceil(0.5)) # 1
+
+# 소수점 버림
+# 해당 값에서 0에 가까운 정수 반환
+import math
+print(math.trunc(-1.5)) # -1
+
+# 소수점 내림
+# 해당 값에서 - 방향으로 가장 가까운 정수 반환
+import math
+print(math.floor(-1.5)) # -2
