cea16df86b01446ffb6b0885c65cb0de9cd09035
nonasking <nonasking@gmail.com>
2023-08-22 15:53:42 +0900

    add modules

--- calculation/calculation.py
+++ calculation/calculation.py
@@ -1,3 +1,12 @@
 # 나누기
 print(8//2, 8%2)
 print(divmod(8,2))
+
+# 절댓값
+print(abs(-1)) # 1
+
+# 소수점 반올림
+print(round(0.6)) # 1
+# 원래 수와 올림/버림했을 때의 차이가 같은 경우 짝수 값 반환
+print(round(0.5)) # 0
+print(round(1.5)) # 2
