ac0caad71583b65fb7791ae6246ace60e8ca5fce
nonasking <nonasking@gmail.com>
2023-08-27 12:12:03 +0900

    add method

--- datetime/datetime.py
+++ datetime/datetime.py
@@ -1,3 +1,10 @@
 # 문자를 날짜로
 from datetime import datetime
 print(datetime.strptime('05:34','%H:%M')) # 1900-01-01 05:34:00
+
+# 날짜끼리의 차 구하기
+import datetime
+day1 = datetime.datetime(2016,1,1)
+day2 = datetime.datetime(2016,2,1)
+print(day2-day1)        # 31 days, 0:00:00
+print((day2-day1).days) # 31
