13c296caf929c8a4b6a8d4c917ebfdd712f6c9ba
nonasking <nonasking@gmail.com>
2023-08-29 16:35:15 +0900

    add method

--- calculation/calculation.py
+++ calculation/calculation.py
@@ -29,3 +29,14 @@ print(math.trunc(-1.5)) # -1
 # 해당 값에서 - 방향으로 가장 가까운 정수 반환
 import math
 print(math.floor(-1.5)) # -2
+
+# 소수의 개수
+def get_divisors_count(n):
+  answer = 0
+  s = int(math.sqrt(n))
+  for i in range(1,s+1):
+    if n % i == 0:
+      answer += 1
+  if n == s**2 :
+    return answer * 2 - 1
+  return answer * 2
