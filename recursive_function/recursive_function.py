ce4a5917b362f2e7059da77bb4ac92695d743cb4
nonasking <nonasking@gmail.com>
2023-09-09 10:41:01 +0900

    edit example

--- recursive_function/recursive_function.py
+++ recursive_function/recursive_function.py
@@ -1,13 +1,12 @@
 # 재귀 함수 기본 예시
-from collections import deque
-queue = deque()
-queue.append(5) # 값 추가
-queue.popleft() # 먼저 들어간 값 제거
-...
 
-print(queue) # 먼저 들어온 순서대로 출력
-queue.reverse() # 큐 안의 값들 순서 뒤집기
-print(queue) # 나중에 들어온 값부터 출력
+# 피보나치 수열 예시
+def fibo(x):
+	if x == 1 or x == 2:
+    	return 1
+    return fibo(x-1) + fibo(x-2)
+
+print(fibo(4)) # 수열의 4번째 값(a4) 구하기
 
 # 팩토리얼 구현 예시
 def factorial(n):
