6df08a92a9471f4359fd897aca78908b8a8219e1
nonasking <nonasking@gmail.com>
2023-09-11 19:22:31 +0900

    add example

---
+++ string/examples/caesar_cipher.py
@@ -0,0 +1,25 @@
+def convert_alphabet(alphabet, n):
+    upper_alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lower_alphabets = "abcdefghijklmnopqrstuvwxyz"
+    
+    if alphabet == ' ':
+        return ' '
+    
+    if alphabet.isupper():
+        index = upper_alphabets.find(alphabet)+n
+        if index >= len(upper_alphabets):
+             index -= len(upper_alphabets)
+        return upper_alphabets[index]
+    else:
+        index = lower_alphabets.find(alphabet)+n
+        if index >= len(lower_alphabets):
+             index -= len(lower_alphabets)
+        return lower_alphabets[index]
+        
+    
+
+def solution(s, n):
+    result = [convert_alphabet(alphabet, n) for alphabet in s]
+    answer = ''.join(result)
+    
+    return answer
