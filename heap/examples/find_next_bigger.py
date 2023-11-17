fbda53d181fd57a1a6d1685d1c08aa98966cea6b
nonasking <nonasking@gmail.com>
2023-11-17 15:08:30 +0900

    add example

--- heap/examples/find_next_bigger.py
+++ heap/examples/find_next_bigger.py
@@ -11,3 +11,24 @@
 
 # def solution(numbers):
 #     return [find_bigger(i, numbers) for i in range(len(numbers))]
+
+
+# def solution(numbers):    
+#     result = []    
+    
+#     for i, v in enumerate(numbers):
+#         j = i + 1
+#         while True:
+#             if v >= max(numbers[i:]):
+#                 result.append(-1)
+#                 break
+#             elif j == len(numbers):
+#                 result.append(-1)
+#                 break
+#             elif numbers[j] > v:
+#                 result.append(numbers[j])
+#                 break
+#             else:
+#                 j += 1
+                
+#     return result
